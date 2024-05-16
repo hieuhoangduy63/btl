20ca019bf877ae772395c8b030c531d4d03bb0e0
hieuhoangduy63 <duyhieuhoanglc@gmail.com>
2024-05-17 01:04:58 +0700

    update do hoa

--- entity/Alien.java
+++ entity/Alien.java
@@ -4,13 +4,15 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import main.Panel;
-
+import javax.swing.ImageIcon;
+import javax.swing.*;
 
 
 public class Alien extends Sprite {
     public int x0;
     Panel p;
     public boolean sangphai=true;
+    private ImageIcon alienSkin1;
     public Alien(int x,int y,int x0,Panel p){
         this.x0=x0;
         this.x=x;
@@ -46,8 +48,8 @@ public class Alien extends Sprite {
     }
     public void draw(Graphics2D g2){
         if(alive==true){
-        g2.setColor(Color.white);
-        g2.fillRect(x,y,width,height);
+        alienSkin1 = new ImageIcon(getClass().getResource("/Image/AlienSkin1.gif"));
+        g2.drawImage(alienSkin1.getImage(), x, y, null);
         }
     }
 }
