24e33c1d4c9e8c6710c651d5d815c3bbffec5f90
minhducht239 <163428001+minhducht239@users.noreply.github.com>
2024-05-24 00:09:54 +0700

    add icon life và sửa vị trí life

--- main/Panel.java
+++ main/Panel.java
@@ -125,18 +125,16 @@ public void run() {
     }
 }
     //Bộ đếm mạng
+    private ImageIcon lifeIcon;
     public void drawLives(Graphics2D g2) {
-        // int livesX = 300;
-        // int livesY = 25;
-        // int livesWidth = 32;
-        // int livesHeight = 32;
+         int livesX = 20;
+         int livesY = 60;
+         int livesWidth = 40;
     
-        // for (int i = 0; i < player.getLife(); i++) {
-        //     g2.setColor(Color.RED);
-        //     g2.fillRect(livesX + i * livesWidth, livesY, livesWidth, livesHeight);
-        //     g2.setColor(Color.WHITE);
-        //     g2.drawRect(livesX + i * livesWidth, livesY, livesWidth, livesHeight);
-        // }
+        for (int i = 0; i < player.getLife(); i++) {
+            lifeIcon = new ImageIcon(getClass().getResource("/Image/shipLife.png"));
+            g2.drawImage(lifeIcon.getImage(),livesX + i * livesWidth, livesY, null);
+         }
     }
     //kiểm tra đạn có trúng không
     public void checkDestroyed(){
